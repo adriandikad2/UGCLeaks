5ca0f3de0d2bf41874454a55537237f281f20975
Adrian Dika <adriandikad2@gmail.com>
2026-07-24 16:12:18 +0700

    cloudflare migration

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  outputFileTracingIncludes: {
+    "**/*": [
+      "./node_modules/pg-cloudflare/dist/**",
+      "./node_modules/pg-cloudflare/esm/**",
+    ],
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
